bb4bebe5eeedfa395e0705cfa2adb48dedc31ecd
ttrreevvoorr <ttrreevvoorr@zulfiqar.zulfiqar.net>
2022-11-02 14:10:38 -0700

    Remove infinite option from poll

--- src/slashCommands/poll.js
+++ src/slashCommands/poll.js
@@ -7,7 +7,7 @@ module.exports = {
   .setDescription("Create a poll with numerous options, up to 10.")
   .addStringOption(option => option.setName("question").setDescription("What are you polling for?").setRequired(true))
   .addStringOption(option => option.setName("choices").setDescription("Poll choices separated by commas").setRequired(true))
-  .addIntegerOption(option => option.setName("expires").setDescription("In how many seconds does this expire? 0 is infinite. Default: 30").setRequired(false)),
+  .addIntegerOption(option => option.setName("expires").setDescription("In how many seconds does this expire? Default: 30").setRequired(false)),
 
   async execute(interaction) {
     await interaction.deferReply({ ephemeral: true })
